cbeeaf8e231c244104b94db7ce95a8fff8a28ca6
Oliver Steele <steele@osteele.com>
2019-09-22 10:20:00 +0800

    Send frame count and number; ignore excerpt files; report disconnections cleanly

--- server.py
+++ server.py
@@ -1,32 +1,47 @@
 #!/usr/bin/env python
 
+import asyncio
+import json
 from pathlib import Path
 from time import sleep
-import asyncio
+
 import websockets
-import json
+from websockets.exceptions import ConnectionClosedError
 
 HOSTNAME = "localhost"
 PORT_NUMBER = 8765
 
 data_dir = Path("./build")
-data_path = sorted(data_dir.glob("*.json"))[0]
+data_paths = sorted(
+    p for p in data_dir.glob("*.json") if not p.stem.endswith("-excerpt")
+)
+data_path = data_paths[0]
+
 records = json.load(data_path.open())
-print(f"Serving data from {data_path.name} ({len(records)} rows)")
+print(f"Serving data from {data_path.name!r} ({len(records)} rows)")
 
 
-async def echo(websocket, path):
-    async for message in websocket:
-        req = json.loads(message)
-        ix = req["cursor"]
-        ix %= len(records)
-        print("request", ix, "/", len(records))
-        row = records[ix]
-        await websocket.send(json.dumps(row))
+async def handler(websocket, path):
+    try:
+        async for message in websocket:
+            req = json.loads(message)
+            ix = req["frame_no"] % len(records)
+            # print("request", ix, "/", len(records))
+            data = {
+                "frame_count": len(records),
+                "frame_number": ix,
+                "pose": records[ix],
+            }
+            await websocket.send(json.dumps(data))
+    except ConnectionClosedError:
+        print("Client closed connection")
 
-
-start_server = websockets.serve(echo, HOSTNAME, PORT_NUMBER)
 
 print(f"Serving ws://{HOSTNAME}:{PORT_NUMBER}")
+start_server = websockets.serve(handler, HOSTNAME, PORT_NUMBER)
 asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
+try:
+    asyncio.get_event_loop().run_forever()
+except KeyboardInterrupt:
+    print("Shutting down")
+    pass
